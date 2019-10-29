acdfa41f3dc09f1796c0751c193fe4155e276200
Paolo <baioni2002@yahoo.fr>
2019-10-29 21:46:47 +0100

    use database.properties

--- appTemplate/src/main/java/app/main/LaunchApplication.java
+++ appTemplate/src/main/java/app/main/LaunchApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ServletComponentScan
+@PropertySource("classpath:database.properties")
 @SpringBootApplication(scanBasePackages = "app")
 @EnableJpaRepositories("app.persistence.repo")
 @EntityScan("app.persistence.model")
