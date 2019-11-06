70498091bdb0c9787b0f04b066a0683edd0cd4a8
Paolo <baioni2002@yahoo.fr>
2019-11-06 18:43:43 +0100

    fix user properties

--- appTemplate/src/main/java/app/persistence/properties/UsersProperties.java
+++ appTemplate/src/main/java/app/persistence/properties/UsersProperties.java
@@ -6,36 +6,36 @@ import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource("classpath:users.properties")
-@ConfigurationProperties
+@ConfigurationProperties(prefix="user")
 public class UsersProperties {
 
-	private String userProperty1;
+	private String property1;
 	
-	private String userProperty2;
+	private String property2;
 	
 	public UsersProperties() {
 		
 	}
 
-	public String getDummyproperty1() {
-		return userProperty1;
+	public String getProperty1() {
+		return property1;
 	}
 
-	public void setDummyproperty1(String userproperty1) {
-		this.userProperty1 = userproperty1;
+	public void setProperty1(String property1) {
+		this.property1 = property1;
 	}
 
-	public String getDummyproperty2() {
-		return userProperty2;
+	public String getProperty2() {
+		return property2;
 	}
 
-	public void setDummyproperty2(String userproperty2) {
-		this.userProperty2 = userproperty2;
+	public void setProperty2(String property2) {
+		this.property2 = property2;
 	}
 
 	@Override
 	public String toString() {
-		return "UsersProperties [userProperty1=" + userProperty1 + ", userProperty2=" + userProperty2 + "]";
+		return "UsersProperties [property1=" + property1 + ", property2=" + property2 + "]";
 	}
 
 }
